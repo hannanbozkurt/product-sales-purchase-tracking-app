8dd057f6363ae31ccdab186460665bd06749d7ba
Hannan Bozkurt <hannanbozkurt@gmail.com>
2022-10-22 12:29:56 +0300

    Product-Sales-Purchase-Tracking-App

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <app-header></app-header>
+    <transition name="fade" mode="out-in">
+      <router-view></router-view >
+    </transition>
+    <app-footer></app-footer>
+  </div>
+</template>
+
+<script>
+import Header from "./components/shared/Header.vue";
+import Footer from "./components/shared/Footer.vue";
+
+export default {
+  components: {
+    appHeader: Header,
+    appFooter: Footer,
+  },
+  created() {
+    this.$store.dispatch("getTradeResult");
+    this.$store.dispatch("initApp");
+  },
+};
+</script>
+
+<style>
+body{
+  background-color: #a7a0bb5c ;
+}
+
+.fade-enter {
+  opacity: 0;
+}
+.fade-enter-active {
+  transition: opacity 0.3s ease-out;
+}
+.fade-leave {
+  opacity: 0;
+}  
+
+.fade-leave-active {
+  transition: opacity 0.3s ease-out;
+  opacity: 0;
+}
+</style>
